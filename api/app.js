fc21a3984c40efac9263581b6b4124e0eae46a2d
Gerald Haxhi <gerald.haxhi@softup.co>
2021-02-24 22:54:49 +0100

    API: Simple endpoints for testing DB connection

--- api/app.js
+++ api/app.js
@@ -1,9 +1,27 @@
 const Express = require('express');
 const BodyParser = require('body-parser');
 const Cors = require('cors');
+const { Pool } = require('pg');
+
+const config = require('./config');
 
 const app = Express();
 const router = new Express.Router();
+
+const pgClient = new Pool({
+  host: config.dbHost,
+  port: config.dbPort,
+  user: config.dbUser,
+  password: config.dbPassword,
+  database: config.dbName,
+});
+
+pgClient.on('error', () => {
+  console.log('Error connecting to Postgres');
+});
+
+pgClient.query('CREATE TABLE IF NOT EXISTS books (title varchar(128))')
+  .catch((err) => console.log(err));
 
 app.use(BodyParser.json({ limit: '10mb' }));
 app.use(BodyParser.urlencoded({ limit: '10mb', extended: false }));
@@ -17,6 +35,33 @@ router.route('/health').get(
       version: process.env.npm_package_version,
       status: 'OK',
     });
+  }
+);
+
+router.route('/books').get(
+  async (_req, res) => {
+    try {
+      const result = await pgClient.query('SELECT * FROM books');
+      res.status(200).json(result.rows);
+    } catch (e) {
+      console.log(e);
+      res.status(500).json(e);
+    }
+  }
+);
+
+router.route('/books').post(
+  async (req, res) => {
+    try {
+      const result = await pgClient.query(
+        'INSERT INTO books(title) VALUES($1) RETURNING *',
+        [req.body.title]
+      );
+      res.status(201).json(result.rows[0]);
+    } catch (e) {
+      console.log(e);
+      res.status(500).json(e);
+    }
   }
 );
 
--- api/config.js
+++ api/config.js
@@ -1,3 +1,8 @@
 module.exports = {
   port: 5000,
+  dbHost: process.env.DATABASE_HOST,
+  dbPort: process.env.DATABASE_PORT,
+  dbUser: process.env.DATABASE_USER,
+  dbPassword: process.env.DATABASE_PASSWORD,
+  dbName: process.env.DATABASE_NAME,
 }
