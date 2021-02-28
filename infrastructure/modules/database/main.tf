44cfa90894d001b32a71b951ba9f646133832adb
Gerald Haxhi <gerald.haxhi@softup.co>
2021-02-28 13:23:34 +0100

    INFRASTRUCTURE: Fixes in DB module

--- infrastructure/modules/database/main.tf
+++ infrastructure/modules/database/main.tf
@@ -8,20 +8,15 @@ resource "aws_db_parameter_group" "db-parameters" {
   name        = "${var.APP_NAME}-${var.ENV}-db-parameters"
   family      = "postgres12"
   description = "RDS parameter group"
-
-  parameter {
-    name  = "max_allowed_packet"
-    value = "16777216"
-  }
 }
 
 resource "aws_db_instance" "db" {
   allocated_storage       = 20
   engine                  = "postgres"
-  engine_version          = "12.5.1"
+  engine_version          = "12.5"
   instance_class          = "db.t2.micro"
-  identifier              = "postgres"
-  name                    = "${var.APP_NAME}-${var.ENV}-db"
+  identifier              = "${var.APP_NAME}-${var.ENV}-db"
+  name                    = "${var.APP_NAME}${var.ENV}db"
   username                = var.DB_USERNAME
   password                = var.DB_PASSWORD
   db_subnet_group_name    = aws_db_subnet_group.db-subnet.name
