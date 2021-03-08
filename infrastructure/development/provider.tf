47978cbfef1acb5c40181116bb0a7e8dc53bd294
Gerald Haxhi <gerald.haxhi@softup.co>
2021-03-08 22:52:54 +0100

    Github actions config

--- infrastructure/development/provider.tf
+++ infrastructure/development/provider.tf
@@ -3,3 +3,22 @@ provider "aws" {
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  backend "remote" {
+    organization = var.ORGANIZATION
+
+    workspaces {
+      name = var.WORKSPACE_NAME
+    }
+  }
+}
--- infrastructure/development/variables.tf
+++ infrastructure/development/variables.tf
@@ -58,3 +58,11 @@ variable "SNS_EMAIL_RECEIVER" {
 variable "SNS_STACK_NAME" {
   default = "sns-stack-102947586"
 }
+
+variable "ORGANIZATION_NAME" {
+  default = "GH"
+}
+
+variable "WORKSPACE_NAME" {
+  default = "Terraform-AWS-ECS"
+}
