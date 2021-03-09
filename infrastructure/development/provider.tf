85b56323397b48df74f9a1c1fcc4e7a27de055e8
Gerald Haxhi <gerald.haxhi@softup.co>
2021-03-09 12:48:13 +0100

    Updated github actions config

--- infrastructure/development/provider.tf
+++ infrastructure/development/provider.tf
@@ -1,8 +1,8 @@
-provider "aws" {
-  region     = var.AWS_REGION
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
-}
+# provider "aws" {
+#   region     = var.AWS_REGION
+#   access_key = var.AWS_ACCESS_KEY
+#   secret_key = var.AWS_SECRET_KEY
+# }
 
 terraform {
   required_providers {
