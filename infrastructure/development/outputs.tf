396176ed51de9e5873288379af4c758c5b9a9200
Gerald Haxhi <gerald.haxhi@softup.co>
2021-03-09 11:36:46 +0100

    INFRASTRUCTURE: Added storage module outputs

---
+++ infrastructure/development/outputs.tf
@@ -0,0 +1,7 @@
+output "WEB_CLIENT_DOMAIN_DEV" {
+  value = module.storage.web-client-domain-name
+}
+
+output "WEB_ADMIN_DOMAIN_DEV" {
+  value = module.storage.web-admin-domain-name
+}
--- infrastructure/modules/storage/outputs.tf
+++ infrastructure/modules/storage/outputs.tf
@@ -1,1 +1,7 @@
-// TODO
+output "web-client-domain-name" {
+  value = aws_s3_bucket.web-client.bucket_domain_name
+}
+
+output "web-admin-domain-name" {
+  value = aws_s3_bucket.web-admin.bucket_domain_name
+}
