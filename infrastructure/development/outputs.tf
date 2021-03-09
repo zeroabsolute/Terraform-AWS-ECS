e6fe8e010ab013c9c86e05afba68c83fe0228faa
Gerald Haxhi <gerald.haxhi@softup.co>
2021-03-09 23:34:58 +0100

    Added S3 deploy

--- infrastructure/development/outputs.tf
+++ infrastructure/development/outputs.tf
@@ -5,3 +5,11 @@ output "WEB_CLIENT_DOMAIN_DEV" {
 output "WEB_ADMIN_DOMAIN_DEV" {
   value = module.storage.web-admin-domain-name
 }
+
+output "WEB_CLIENT_BUCKET_DEV" {
+  value = module.storage.web-admin-bucket-name
+}
+
+output "WEB_ADMIN_BUCKET_DEV" {
+  value = module.storage.web-admin-bucket-name
+}
--- infrastructure/modules/storage/outputs.tf
+++ infrastructure/modules/storage/outputs.tf
@@ -5,3 +5,11 @@ output "web-client-domain-name" {
 output "web-admin-domain-name" {
   value = aws_s3_bucket.web-admin.bucket_domain_name
 }
+
+output "web-client-bucket-name" {
+  value = aws_s3_bucket.web-client.id
+}
+
+output "web-admin-bucket-name" {
+  value = aws_s3_bucket.web-admin.id
+}
