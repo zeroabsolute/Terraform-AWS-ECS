5e633bc817e9c225e958e255f46c4fc3d429d385
Gerald Haxhi <gerald.haxhi@softup.co>
2021-03-11 23:40:26 +0100

    INFRASTRUCTURE: Fixes

--- infrastructure/modules/containers/main.tf
+++ infrastructure/modules/containers/main.tf
@@ -4,53 +4,53 @@ resource "aws_ecr_repository" "ecr-repository" {
   name = "${var.APP_NAME}-ecr-repository"
 }
 
-# # ECS cluster
+# ECS cluster
 
-# resource "aws_ecs_cluster" "ecs-cluster" {
-#   name = "${var.APP_NAME}-${var.ENV}-ecs-cluster"
-# }
+resource "aws_ecs_cluster" "ecs-cluster" {
+  name = "${var.APP_NAME}-${var.ENV}-ecs-cluster"
+}
 
-# # Task definition 
+# Task definition 
 
-# data "template_file" "task-definition-template" {
-#   template = file("${path.module}/templates/app.json.tpl")
-#   vars = {
-#     REPOSITORY_URL       = replace(aws_ecr_repository.ecr-repository.repository_url, "https://", "")
-#     CONTAINER_PORT       = var.CONTAINER_PORT
-#     APP_NAME             = var.APP_NAME
-#     DATABASE_HOST        = var.DATABASE_HOST
-#     DATABASE_PORT        = var.DATABASE_PORT
-#     DATABASE_USER        = var.DATABASE_USER
-#     DATABASE_PASSWORD    = var.DATABASE_PASSWORD
-#     DATABASE_NAME        = var.DATABASE_NAME
-#     CLOUDWATCH_LOG_GROUP = var.CLOUDWATCH_LOG_GROUP
-#     AWS_REGION           = var.AWS_REGION
-#   }
-# }
+data "template_file" "task-definition-template" {
+  template = file("${path.module}/templates/app.json.tpl")
+  vars = {
+    REPOSITORY_URL       = replace(aws_ecr_repository.ecr-repository.repository_url, "https://", "")
+    CONTAINER_PORT       = var.CONTAINER_PORT
+    APP_NAME             = var.APP_NAME
+    DATABASE_HOST        = var.DATABASE_HOST
+    DATABASE_PORT        = var.DATABASE_PORT
+    DATABASE_USER        = var.DATABASE_USER
+    DATABASE_PASSWORD    = var.DATABASE_PASSWORD
+    DATABASE_NAME        = var.DATABASE_NAME
+    CLOUDWATCH_LOG_GROUP = var.CLOUDWATCH_LOG_GROUP
+    AWS_REGION           = var.AWS_REGION
+  }
+}
 
-# resource "aws_ecs_task_definition" "task-definition" {
-#   family                = var.APP_NAME
-#   container_definitions = data.template_file.task-definition-template.rendered
-# }
+resource "aws_ecs_task_definition" "task-definition" {
+  family                = var.APP_NAME
+  container_definitions = data.template_file.task-definition-template.rendered
+}
 
-# # ECS service
+# ECS service
 
-# resource "aws_ecs_service" "ecs-service" {
-#   name            = "${var.APP_NAME}-${var.ENV}-ecs-service"
-#   cluster         = aws_ecs_cluster.ecs-cluster.id
-#   task_definition = aws_ecs_task_definition.task-definition.arn
-#   desired_count   = var.ASG_MAX_SIZE
-#   iam_role        = var.ECS_SERVICE_IAM_ROLE
-#   depends_on      = [var.ECS_SERVICE_IAM_POLICY_ATTACHMENT]
+resource "aws_ecs_service" "ecs-service" {
+  name            = "${var.APP_NAME}-${var.ENV}-ecs-service"
+  cluster         = aws_ecs_cluster.ecs-cluster.id
+  task_definition = aws_ecs_task_definition.task-definition.arn
+  desired_count   = var.ASG_MAX_SIZE
+  iam_role        = var.ECS_SERVICE_IAM_ROLE
+  depends_on      = [var.ECS_SERVICE_IAM_POLICY_ATTACHMENT]
 
-#   load_balancer {
-#     target_group_arn = var.TARGET_GROUP_ARN
-#     container_name   = var.APP_NAME
-#     container_port   = var.CONTAINER_PORT
-#   }
-#   lifecycle {
-#     ignore_changes = [task_definition]
-#   }
-# }
+  load_balancer {
+    target_group_arn = var.TARGET_GROUP_ARN
+    container_name   = var.APP_NAME
+    container_port   = var.CONTAINER_PORT
+  }
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+}
 
 
--- infrastructure/modules/containers/outputs.tf
+++ infrastructure/modules/containers/outputs.tf
@@ -1,6 +1,6 @@
-# output "ecs-cluster-name" {
-#   value = aws_ecs_cluster.ecs-cluster.name
-# }
+output "ecs-cluster-name" {
+  value = aws_ecs_cluster.ecs-cluster.name
+}
 
 output "ecr-repository-name" {
   value = aws_ecr_repository.ecr-repository.name
